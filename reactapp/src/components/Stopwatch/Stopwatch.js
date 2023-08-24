81f1ae55efec55a77636c06b1fba5e1b95f71ddb
Iamneo <techadmin@iamneo.ai>
2023-08-24 08:37:05 +0000

    Backup Commit

---
+++ reactapp/src/components/Stopwatch/Stopwatch.js
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from "react";
+
+
+const Stopwatch = () => {
+    const [timer, setTimer] = useState(0)
+    const [isActive, setIsActive] = useState(false)
+    const [isPaused, setIsPaused] = useState(false)
+    const increment = useRef(null)
+  
+    const handleStart = () => {
+      setIsActive(true)
+      setIsPaused(true)
+      increment.current = setInterval(() => {
+        setTimer((timer) => timer + 1)
+      }, 1000)
+    }
+  
+    const handlePause = () => {
+      clearInterval(increment.current)
+      setIsPaused(false)
+    }
+  
+    const handleResume = () => {
+      setIsPaused(true)
+      increment.current = setInterval(() => {
+        setTimer((timer) => timer + 1)
+      }, 1000)
+    }
+  
+    const handleReset = () => {
+      clearInterval(increment.current)
+      setIsActive(false)
+      setIsPaused(false)
+      setTimer(0)
+    }
+  
+    const formatTime = () => {
+      const getSeconds = `0${(timer % 60)}`.slice(-2)
+      const minutes = `${Math.floor(timer / 60)}`
+      const getMinutes = `0${minutes % 60}`.slice(-2)
+      const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+  
+      return `${getHours} : ${getMinutes} : ${getSeconds}`
+    }
+  
+    return (
+      <div className="outer-main">
+        <p className="inner-main">
+        <p className="head">React Stopwatch</p>
+        <p  data-testid="time" className="time">
+            {formatTime()}
+          </p>
+          <div  data-testid="button" >
+            {
+              !isActive && !isPaused ?
+                <button onClick={handleStart} className="button"  data-testid="start" >Start</button>
+                : (
+                  isPaused ? <button onClick={handlePause} className="button" data-testid="pause">Pause</button> :
+                    <button onClick={handleResume} className="button" data-testid="resume">Resume</button>
+                )
+            }
+            
+            <button onClick={handleReset} disabled={!isActive} className="button" data-testid="reset">Reset
+            </button>
+            </div>
+        </p>
+      </div>
+    );
+  }
+  
+  export default Stopwatch;
